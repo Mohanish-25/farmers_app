3e178aae7eedce1886e2fb7ff9bfc73b927cef32
Mohanish Desale <mohanishdesale@gmail.com>
2025-01-07 10:39:37 +0530

    removed unnecessary container

--- lib/Componets/header.dart
+++ lib/Componets/header.dart
@@ -26,59 +26,53 @@ class Header extends StatelessWidget {
         screenWidth * 0.05; // Spacing as 5% of screen width
     final textFontSize = screenWidth * 0.06; // Font size as 6% of screen width
 
-    return Container(
-      // padding: EdgeInsets.symmetric(
-      //   horizontal: screenWidth * 0.06, // Padding as 7% of screen width
-      //   vertical: screenHeight * 0.04, // Padding as 2% of screen height
-      // ),
-      child: Padding(
-        padding: EdgeInsets.fromLTRB(
-            screenWidth * 0.05, screenHeight * 0.03, screenWidth * 0.05, 0.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Row(
-              children: [
-                Image.asset(
-                  collegeLogo,
-                  width: logoWidth,
-                  height: logoWidth * 1.35, // Maintain aspect ratio
-                  fit: BoxFit.contain,
+    return Padding(
+      padding: EdgeInsets.fromLTRB(
+          screenWidth * 0.05, screenHeight * 0.03, screenWidth * 0.05, 0.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              Image.asset(
+                collegeLogo,
+                width: logoWidth,
+                height: logoWidth * 1.35, // Maintain aspect ratio
+                fit: BoxFit.contain,
+              ),
+              SizedBox(width: spacingBetweenLogos),
+              Text(
+                'PESMCOE ',
+                style: TextStyle(
+                  fontFamily: 'Poppins-SemiBold',
+                  fontSize: textFontSize,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0.15,
+                  color: Colors.black,
+                  height: 1.2,
                 ),
-                SizedBox(width: spacingBetweenLogos),
-                Text(
-                  'PESMCOE ',
-                  style: TextStyle(
-                    fontFamily: 'Poppins-SemiBold',
-                    fontSize: textFontSize,
-                    fontWeight: FontWeight.w400,
-                    letterSpacing: 0.15,
-                    color: Colors.black,
-                    height: 1.2,
-                  ),
+              ),
+              Text(
+                'NSS UNIT',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: textFontSize,
+                  fontWeight: FontWeight.w700,
+                  letterSpacing: 0.15,
+                  color: const Color(0xFF132672),
+                  height: 1.2,
                 ),
-                Text(
-                  'NSS UNIT',
-                  style: TextStyle(
-                    fontFamily: 'Poppins',
-                    fontSize: textFontSize,
-                    fontWeight: FontWeight.w700,
-                    letterSpacing: 0.15,
-                    color: const Color(0xFF132672),
-                    height: 1.2,
-                  ),
-                ),
-              ],
-            ),
-            Image.network(
-              nssLogo,
-              width: nssLogoWidth,
-              height: nssLogoWidth * 1.1, // Maintain aspect ratio
-              fit: BoxFit.contain,
-            ),
-          ],
-        ),
+              ),
+            ],
+          ),
+          Image.network(
+            nssLogo,
+            width: nssLogoWidth,
+            height: nssLogoWidth * 1.1, // Maintain aspect ratio
+            fit: BoxFit.contain,
+          ),
+        ],
       ),
     );
   }
